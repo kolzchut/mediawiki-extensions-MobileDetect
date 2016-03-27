af9f4a41c77cd5996679b05bc7ccb102ec1e9680
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2016-03-28 01:56:41 +0300

    Convert extension to new MediaWiki Extension Registration

--- MobileDetect.php
+++ MobileDetect.php
@@ -1,35 +1,13 @@
 <?php
-
-$wgExtensionCredits['parserhook'][] = array(
-	'name' => 'MobileDetect',
-	'version' => '2.1a',
-	'license-name' => 'GPL-2.0+',
-	'descriptionmsg' => 'mobiledetect-desc',
-	'author' => array( 'Matthew Tran', 'Luis Felipe Schenone', 'Dror S. [FFS]' ),
-	'url' => 'http://www.mediawiki.org/wiki/Extension:MobileDetect',
-);
-
-$wgExtensionMessagesFiles['MobileDetect'] = __DIR__ . '/MobileDetect.i18n.php';
-$wgMessagesDirs['MobileDetect'] = __DIR__ . '/i18n';
-
-$wgAutoloadClasses['MobileDetect'] = __DIR__ . '/MobileDetect.body.php';
-$wgAutoloadClasses['MobileDetectHooks'] = __DIR__ . '/MobileDetect.hooks.php';
-
-$wgHooks['ParserFirstCallInit'][] = 'MobileDetectHooks::onParserFirstCallInit';
-$wgHooks['PageRenderingHash'][] = 'MobileDetectHooks::onPageRenderingHash';
-$wgHooks['BeforePageDisplay'][] = 'MobileDetectHooks::onBeforePageDisplay';
-$wgHooks['RequestContextCreateSkin'][] = 'MobileDetectHooks::onRequestContextCreateSkin';
-
-
-
-// Configuration
-$wgMobileDetectSkin = null; // Set a skin for mobile, e.g. 'SkinVector' for Vector
-
-// @TODO make $wgMobileDetectTabletIsMobile actually work
-$wgMobileDetectTabletIsMobile = false;
-
-
-// Backwards compatibility
-function mobiledetect() {
-	return MobileDetect::isMobile();
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MobileDetect' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['MobileDetect'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for MobileDetect extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the MobileDetect extension requires MediaWiki 1.25+' );
 }
